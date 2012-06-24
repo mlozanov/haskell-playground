222a2711b01ecd7a1eeb0fafe29593fd4a4188fe
mikhail lozanov <mlozanov@gmail.com>
2012-06-24 13:59:01 +0300

    VBO test case

--- Main.hs
+++ Main.hs
@@ -33,6 +33,7 @@ data World = World { cameras :: Cameras
 data RenderState = RenderState { projectionMatrix :: Ptr GLfloat
                                , viewMatrix :: Ptr GLfloat
                                , shaderPrograms :: [ShaderProgram] 
+                               , bufferObjects :: [Vbo]
                                }
 
 main = do 
@@ -47,22 +48,22 @@ main = do
   GL.blendFunc  $= (GL.SrcAlpha, GL.OneMinusSrcAlpha)
   GL.lineWidth  $= 1.5
   -- set the color to clear background
-  GL.clearColor $= Color4 0 0 0 0
+  GL.clearColor $= Color4 0.18 0.18 0.18 1.0
 
   GL.depthFunc $= Just Less
   
-  GL.ambient  (GL.Light 0) $= GL.Color4 0.3 0.3 0.3 1.0
-  GL.diffuse  (GL.Light 0) $= GL.Color4 1.0 1.0 1.0 1.0
+  GL.ambient  (GL.Light 0) $= GL.Color4 0.2 0.2 0.2 1.0
+  GL.diffuse  (GL.Light 0) $= GL.Color4 0.4 0.6 0.8 1.0
   GL.specular (GL.Light 0) $= GL.Color4 0.8 0.8 0.8 1.0
-  GL.lightModelAmbient  $= GL.Color4 0.2 0.2 0.2 1.0
+--  GL.lightModelAmbient  $= GL.Color4 1.0 1.0 1.0 1.0
  
   GL.colorMaterial $= Just (GL.FrontAndBack, GL.AmbientAndDiffuse)
 
   GL.lighting $= GL.Enabled
   GL.light (GL.Light 0) $= GL.Enabled
-  GL.position (GL.Light 0) $= GL.Vertex4 0 10 2.0 1.0
+  GL.position (GL.Light 0) $= GL.Vertex4 0 100.0 (0.0) 0.0
  
-  -- set 2D orthogonal view inside windowSizeCallback because
+  -- set 2D perspective view inside windowSizeCallback because
   -- any change to the Window size should result in different
   -- OpenGL Viewport.
   GLFW.windowSizeCallback $= \ size@(GL.Size w h) ->
@@ -81,7 +82,10 @@ main = do
   viewMatrixArray <- newArray $ replicate 16 (0.0 :: GLfloat)
   defaultProgram <- newProgram "../../data/shaders/default.vert" "../../data/shaders/default.frag" 
   badPrintProgram <- newProgram "../../data/shaders/badprint.vert" "../../data/shaders/badprint.frag" 
-  renderStateRef <- newIORef (RenderState projMatrixArray viewMatrixArray [defaultProgram, badPrintProgram])
+
+  vbo <- Vbo.fromList $ map (* 10.0) (concat [ [x,y,z] | x <- [-2, -1.5 .. 2.0], y <- [-2, -1.5 .. 2.0], z <- [-2.0, -1.9 .. 2.0] ])
+
+  renderStateRef <- newIORef (RenderState projMatrixArray viewMatrixArray [defaultProgram, badPrintProgram] [vbo])
 
   GL.get GL.vendor >>= print
   GL.get GL.renderer >>= print
@@ -98,7 +102,6 @@ main = do
 updateCameras cameras state = loop 
   where
     loop = map (\c -> evalState (simpleFraming c) state) cameras
-      --loop cameras state
 
 -- we start with waitForPress action
 mainLoop world renderState render simulate = loop 0.0 world renderState waitForPress
@@ -114,8 +117,6 @@ mainLoop world renderState render simulate = loop 0.0 world renderState waitForP
       performGC
 
       t1 <- getCPUTime
-
-      --print $ fromIntegral (t1 - t0) / 10^6
 
       -- check whether ESC is pressed for termination
       p <- GLFW.getKey GLFW.ESC
@@ -165,9 +166,6 @@ renderer' t worldRef renderStateRef = do
 
   toGLMatrix Math.perspective >>= (\m -> matrix (Just GL.Projection) $= m)
 
-  GL.lighting $= GL.Enabled
-  GL.light (Light 0) $= GL.Enabled
-
   GL.matrixMode $= GL.Modelview 0
   GL.loadIdentity
 
@@ -181,8 +179,19 @@ renderer' t worldRef renderStateRef = do
 
   world <- readIORef worldRef 
 
+
+  GL.lighting $= GL.Enabled
+  GL.light (Light 0) $= GL.Enabled
+
+
+  let vbo = head $ bufferObjects renderState
+   in let f4 t = withVbo (f3 vbo t) vbo 
+       in withProgram ((shaderPrograms renderState) !! 0) (f4 t)
+
+  f t
+
   withProgram ((shaderPrograms renderState) !! 0) (f1 t)
-  withProgram ((shaderPrograms renderState) !! 1) (f2 t)
+--  withProgram ((shaderPrograms renderState) !! 1) (f2 t)
 
   return ()
 
@@ -192,16 +201,22 @@ f t = preservingMatrix $ do
         GL.color $ color3 1 0 0
         GL.scale (-0.05) 0.05 (0.05 :: GLfloat)
         renderString Fixed8x16 "ROOM"
+        GL.color $ color3 1 1 1
 
 f1 t = preservingMatrix $ do
          GL.translate $ vector3 10.0 0.0 0.0
          GL.rotate (180 * sin t) (vector3 0 (sin t) 1)
-         O.renderObject O.Solid (O.Cube 15.5)
+         O.renderObject O.Solid (O.Cube 10.0)
 
 f2 t = preservingMatrix $ do
          GL.translate $ vector3 (-10.0) 0.0 0.0
          GL.rotate (180 * sin t) (vector3 0 (sin t) 1)
-         O.renderObject O.Solid (O.Cube 15.5)
+         O.renderObject O.Solid (O.Cube 10.0)
+
+f3 vbo t = preservingMatrix $ do
+             GL.translate $ vector3 (-30.0) 0.0 (-100.0)
+             GL.rotate (180 * sin t) (vector3 0 (sin t) 1)
+             drawVbo vbo
 
 simulate' :: GLfloat -> World -> World
 simulate' t world = world { cameras = cs }
