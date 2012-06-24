55f427851efe6587e235b346bf47be3c7bc4fee0
mikhail lozanov <mlozanov@gmail.com>
2012-06-24 13:57:45 +0300

    VBOs working. Module still needs better interface for usability

--- Vbo.hs
+++ Vbo.hs
@@ -1,15 +1,41 @@
 module Vbo where
 
 import Graphics.Rendering.OpenGL 
+import Data.Array.MArray
+import Data.Array.Storable
+import Foreign
 
-data Vbo = Vbo { buffer :: GLint }
+data Vbo = Vbo { bufferObject :: BufferObject
+               , bufferSize :: GLint 
+               }
 
 newVbo :: IO Vbo
-newVbo = return $ Vbo 0
+newVbo = do [array] <- genObjectNames 1
+            bindBuffer ArrayBuffer $= Just array
+            bindBuffer ArrayBuffer $= Nothing
+            --reportErrors
+            return $ Vbo array 0
 
-withVbo :: Vbo -> IO ()
-withVbo vbo = return ()
+withVbo :: IO () -> Vbo -> IO ()
+withVbo actions vbo = bindBuffer ArrayBuffer $= Just (bufferObject vbo) >> actions >> bindBuffer ArrayBuffer $= Nothing >> return ()
 
-fromList :: [GLfloat] -> Vbo
-fromList l = Vbo 0
+fromList :: [GLfloat] -> IO Vbo
+fromList l = do vbo <- newVbo
+                let size = 4 * length l
+                let f l = do arr <- newListArray (0, size - 1) l
+                             withStorableArray arr (\ptr -> bufferData ArrayBuffer $= (toEnum size, ptr, StaticDraw))
+                             return ()
+
+                withVbo (f l) vbo
+                return vbo { bufferSize = toEnum size }
+
+
+drawVbo :: Vbo -> IO ()
+drawVbo vbo = do arrayPointer VertexArray $= VertexArrayDescriptor 3 Float 0 (offset 0)
+                 --arrayPointer ColorArray $= VertexArrayDescriptor 4 Float 28 (offset 12)
+                 clientState VertexArray $= Enabled
+                 --clientState ColorArray $= Enabled
+
+                 drawArrays LineStrip 0 $ (toEnum . fromEnum) (bufferSize vbo)
+    where offset a = plusPtr nullPtr a
 
