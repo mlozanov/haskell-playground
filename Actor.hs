380a2adbc40bbb15fe915d7dd834933756db444b
mikhail lozanov <mlozanov@gmail.com>
2012-11-07 23:51:10 +0200

    Actor data or player and enemies. draw functions will go in Rendering module

--- Actor.hs
+++ Actor.hs
@@ -3,23 +3,22 @@ module Actor where
 import Graphics.Rendering.OpenGL as GL
 import Graphics.UI.GLUT.Objects as O
 
+import Math
 import Graphics
 
 data Actor = SimpleActor
            | Actor [Double]
+           | Player { playerName :: String
+                    , playerPosition :: Vector Float 
+                    , playerOrientation :: Quaternion Float
+                    }
+           | Enemy { enemyName :: String
+                   , enemyPosition :: Vector Float 
+                   , enemyOrientation :: Quaternion Float
+                   }
              deriving (Eq, Ord, Show)
 
 type Actors = [Actor]
 
-draw :: Actor -> IO ()
-draw SimpleActor = do
-  GL.color $ color3 1 1 0
-  O.renderObject O.Solid (O.Cube 2.0)
-
-draw (Actor actor) = preservingMatrix $ do
-  GL.color $ color3 1 0 0
-  O.renderObject O.Solid (O.Cube 1.5)
-  GL.translate $ vector3 0 (-1.0) 0
-  O.renderObject O.Solid (O.Cube 1.3)
 
 
