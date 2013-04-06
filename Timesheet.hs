50ace7b286e2d49e196176b416829d2ce3e20c64
mikhail lozanov <mlozanov@gmail.com>
2013-04-06 08:47:01 +0300

    show instance for callbacks in timesheet to ease debugging

--- Timesheet.hs
+++ Timesheet.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Timesheet where
 
 import qualified Data.Map as M
@@ -11,6 +13,9 @@ import World
 type Timesheet a = M.Map a Actors
 type Events a = (a, (a -> Actors))
 
+instance Show ([Float] -> Actors) where
+  show callback = "callback"
+
 executeEvent :: (Eq a, Ord a) => Timesheet a -> a -> Actors
 executeEvent timesheet timestamp = fromMaybe' $ M.lookup timestamp timesheet
   where fromMaybe' (Just v) = v
@@ -20,10 +25,11 @@ newTimesheetRow :: (Eq a, Ord a) => Timesheet a -> (a, Actors) -> Timesheet a
 newTimesheetRow timesheet (k, actors) = M.insert k actors timesheet 
 
 lineOfEnemies origin = map enemy positionsAndVelocities
-  where positionsAndVelocities = [ [x,y,0.0] | x <- [8.0], y <- [-1.0, 0.0, 1.0] ]
+  where positionsAndVelocities = [ [x,y,0.0] | x <- [4.0], y <- [-1.5, 0.0, 1.5] ]
         enemy p = newEnemy { enemyPosition = (mulScalarVec 60.0 (addVec origin p))
                            , enemyVelocity = [-60,0.0,0.0]
                            , enemyOrientation = fromAxisAngleQ 0.0 0.0 1.0 (degToRad 180) 
+                           , enemyOmega = [0.0, 0.0, -3.5]
                            }
 
 lineOfEnemiesHorizondal origin = map enemy positionsAndVelocities
@@ -39,7 +45,7 @@ circleOfEnemies v = map defaultEnemy positions
 
 boss = map (\e -> e { enemyAge = 50.0, enemyVelocity = mulScalarVec 0.2 (enemyVelocity e), enemyShootingRate = 0.8, enemyTag = Boss1 }) (circleOfEnemies 12)
 
-stageOneLinearWaves = [ (x, lineOfEnemiesHorizondal) | x <- [10, 220 .. 2500] ]
+stageOneLinearWaves = concat [ [(x, lineOfEnemiesHorizondal), (x + 100, lineOfEnemies)] | x <- [10, 220 .. 2500] ]
 
 stageOneTimesheet :: World -> Timesheet Int
 stageOneTimesheet world = M.fromList $ (initialWave world) ++ [(2600, circleOfEnemies 3), (2820, boss)]
