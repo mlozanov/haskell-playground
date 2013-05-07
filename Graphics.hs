d71fe6c47f6b02bdb6ea6eb84e0f96be5535d7b6
mikhail lozanov <mlozanov@gmail.com>
2013-05-07 19:53:17 +0300

    clean up after move to OpenGL 3.2

--- Graphics.hs
+++ Graphics.hs
@@ -5,18 +5,6 @@ import Foreign.Ptr
 import Foreign.Marshal.Array
 import Math
 
-vertex3 :: GLfloat -> GLfloat -> GLfloat -> GL.Vertex3 GLfloat
-vertex3 = GL.Vertex3
- 
-vector3 :: GLfloat -> GLfloat -> GLfloat -> GL.Vector3 GLfloat
-vector3 = GL.Vector3
-
-fromVector :: Vector Float -> GL.Vector3 GLfloat
-fromVector [x,y,z] = vector3 (toGLfloat x) (toGLfloat y) (toGLfloat z)
- 
-color3 :: GLfloat -> GLfloat -> GLfloat -> GL.Color3 GLfloat
-color3 = GL.Color3
-
 toGLfloat :: Float -> GLfloat
 toGLfloat x = realToFrac x
 
